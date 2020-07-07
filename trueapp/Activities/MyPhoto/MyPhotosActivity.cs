1ef0f1f7dbd9812514634c6b2b7c3a5daafab07d
Link-Cruise <279988706@qq.com>
2020-07-07 14:40:51 +0800

    [add MyPhotoActivity.cs]

---
+++ trueapp/Activities/MyPhoto/MyPhotosActivity.cs
@@ -0,0 +1,493 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Ads;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Bumptech.Glide.Integration.RecyclerView;
+using Bumptech.Glide.Util;
+using Newtonsoft.Json;
+using ObeeNetwork.Activities.MyPhoto.Adapters;
+using ObeeNetwork.Helpers.Ads;
+using ObeeNetwork.Helpers.Controller;
+using ObeeNetwork.Helpers.Model;
+using ObeeNetwork.Helpers.Utils;
+using ObeeNetworkClient.Classes.Album;
+using ObeeNetworkClient.Classes.Posts;
+using ObeeNetworkClient.Requests;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace ObeeNetwork.Activities.MyPhoto
+{
+    [Activity(Icon = "@mipmap/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.Locale | ConfigChanges.UiMode | ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MyPhotosActivity : AppCompatActivity
+    {
+        #region Variables Basic
+
+        private MyPhotosAdapter MAdapter;
+        private SwipeRefreshLayout SwipeRefreshLayout;
+        private RecyclerView MRecycler;
+        private GridLayoutManager LayoutManager;
+        private ViewStub EmptyStateLayout;
+        private View Inflated;
+        private RecyclerViewOnScrollListener MainScrollEvent;
+        private AdView MAdView;
+        
+        #endregion
+
+        #region General
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                SetTheme(AppSettings.SetTabDarkTheme ? Resource.Style.MyTheme_Dark_Base : Resource.Style.MyTheme_Base);
+
+                Methods.App.FullScreenApp(this);
+
+                // Create your application here
+                SetContentView(Resource.Layout.RecyclerDefaultLayout);
+
+                //Get Value And Set Toolbar
+                InitComponent();
+                InitToolbar();
+                SetRecyclerViewAdapters();
+
+                LoadImage();
+
+                AdsGoogle.Ad_Interstitial(this);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+                AddOrRemoveEvent(true);
+                MAdView?.Resume();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnPause()
+        {
+            try
+            {
+                base.OnPause();
+                AddOrRemoveEvent(false);
+                MAdView?.Pause();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public override void OnTrimMemory(TrimMemory level)
+        {
+            try
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+                base.OnTrimMemory(level);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public override void OnLowMemory()
+        {
+            try
+            {
+                GC.Collect(GC.MaxGeneration);
+                base.OnLowMemory();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            try
+            {
+                if (MAdapter.MyPhotosList.Count > 0)
+                    ListUtils.ListCachedDataMyPhotos = MAdapter.MyPhotosList;
+
+                DestroyBasic();
+
+                base.OnDestroy();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+        #region Menu 
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish();
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        #endregion
+
+        #region Functions
+
+        private void InitComponent()
+        {
+            try
+            {
+                MRecycler = (RecyclerView)FindViewById(Resource.Id.recyler);
+                EmptyStateLayout = FindViewById<ViewStub>(Resource.Id.viewStub);
+
+                SwipeRefreshLayout = (SwipeRefreshLayout)FindViewById(Resource.Id.swipeRefreshLayout);
+                SwipeRefreshLayout.SetColorSchemeResources(Android.Resource.Color.HoloBlueLight, Android.Resource.Color.HoloGreenLight, Android.Resource.Color.HoloOrangeLight, Android.Resource.Color.HoloRedLight);
+                SwipeRefreshLayout.Refreshing = true;
+                SwipeRefreshLayout.Enabled = true;
+                SwipeRefreshLayout.SetProgressBackgroundColorSchemeColor(AppSettings.SetTabDarkTheme ? Color.ParseColor("#424242") : Color.ParseColor("#f7f7f7"));
+ 
+
+                MAdView = FindViewById<AdView>(Resource.Id.adView);
+                AdsGoogle.InitAdView(MAdView, MRecycler);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void InitToolbar()
+        {
+            try
+            {
+                var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+                if (toolbar != null)
+                {
+                    toolbar.Title = GetText(Resource.String.Lbl_MyImages);
+                    toolbar.SetTitleTextColor(Color.White);
+                    SetSupportActionBar(toolbar);
+                    SupportActionBar.SetDisplayShowCustomEnabled(true);
+                    SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+                    SupportActionBar.SetHomeButtonEnabled(true);
+                    SupportActionBar.SetDisplayShowHomeEnabled(true);
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void SetRecyclerViewAdapters()
+        {
+            try
+            {
+                MAdapter = new MyPhotosAdapter(this)
+                {
+                    MyPhotosList = new ObservableCollection<PostDataObject>()
+                };
+                LayoutManager = new GridLayoutManager(this, 2);
+                LayoutManager.SetSpanSizeLookup(new MySpanSizeLookup(4, 1, 1)); //5, 1, 2 
+                MRecycler.SetLayoutManager(LayoutManager);
+                MRecycler.HasFixedSize = true;
+                MRecycler.SetItemViewCacheSize(10);
+                MRecycler.GetLayoutManager().ItemPrefetchEnabled = true;
+                var sizeProvider = new FixedPreloadSizeProvider(10, 10);
+                var preLoader = new RecyclerViewPreloader<PostDataObject>(this, MAdapter, sizeProvider, 10);
+                MRecycler.AddOnScrollListener(preLoader);
+                MRecycler.SetAdapter(MAdapter);
+
+                RecyclerViewOnScrollListener xamarinRecyclerViewOnScrollListener = new RecyclerViewOnScrollListener(LayoutManager);
+                MainScrollEvent = xamarinRecyclerViewOnScrollListener;
+                MainScrollEvent.LoadMoreEvent += MainScrollEventOnLoadMoreEvent;
+                MRecycler.AddOnScrollListener(xamarinRecyclerViewOnScrollListener);
+                MainScrollEvent.IsLoading = false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void AddOrRemoveEvent(bool addEvent)
+        {
+            try
+            {
+                // true +=  // false -=
+                if (addEvent)
+                {
+                    MAdapter.ItemClick += MAdapterOnOnItemClick;
+                    SwipeRefreshLayout.Refresh += SwipeRefreshLayoutOnRefresh; 
+                }
+                else
+                {
+                    MAdapter.ItemClick -= MAdapterOnOnItemClick;
+                    SwipeRefreshLayout.Refresh -= SwipeRefreshLayoutOnRefresh; 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        private void DestroyBasic()
+        {
+            try
+            {
+                MAdView?.Destroy();
+
+                MAdapter = null;
+                SwipeRefreshLayout = null;
+                MRecycler = null;
+                EmptyStateLayout = null;
+                Inflated = null;
+                MainScrollEvent = null;
+                MAdView = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        #endregion
+
+        #region Events
+
+        //Refresh
+        private void SwipeRefreshLayoutOnRefresh(object sender, EventArgs e)
+        {
+            try
+            {
+                MAdapter.MyPhotosList.Clear();
+                MAdapter.NotifyDataSetChanged();
+
+                MainScrollEvent.IsLoading = false;
+
+                StartApiService();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        //Scroll
+        private void MainScrollEventOnLoadMoreEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                //Code get last id where LoadMore >>
+                var item = MAdapter.MyPhotosList.LastOrDefault();
+                if (item != null && !string.IsNullOrEmpty(item.Id) && !MainScrollEvent.IsLoading)
+                    StartApiService(item.Id);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private void MAdapterOnOnItemClick(object sender, MyPhotosAdapterClickEventArgs e)
+        {
+            try
+            {
+                PostDataObject item = MAdapter.GetItem(e.Position);
+                if (item != null)
+                {
+                    ListUtils.ListCachedDataMyPhotos = MAdapter.MyPhotosList;
+                     
+                    var intent = new Intent(this, typeof(MyPhotoViewActivity));
+                    intent.PutExtra("itemIndex", e.Position.ToString()); 
+                    intent.PutExtra("AlbumObject", JsonConvert.SerializeObject(item)); // PostDataObject
+                    StartActivity(intent);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+         
+        #endregion
+         
+        #region Load Image 
+
+        private void LoadImage()
+        {
+            try
+            {
+                if (ListUtils.ListCachedDataMyPhotos.Count > 0)
+                {
+                    MAdapter.MyPhotosList = ListUtils.ListCachedDataMyPhotos;
+                    MAdapter.NotifyDataSetChanged();
+
+                    var item = MAdapter.MyPhotosList.LastOrDefault()?.Id ?? "0";
+                    StartApiService(item);
+                }
+                else
+                    StartApiService();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+         
+        private void StartApiService(string offset = "0")
+        {
+            if (MAdapter.MyPhotosList.Count > 0)
+                ListUtils.ListCachedDataMyPhotos = MAdapter.MyPhotosList;
+
+            if (!Methods.CheckConnectivity())
+                Toast.MakeText(this, GetString(Resource.String.Lbl_CheckYourInternetConnection), ToastLength.Short).Show();
+            else
+                PollyController.RunRetryPolicyFunction(new List<Func<Task>> { () => LoadMyImage(offset) });
+        }
+
+        private async Task LoadMyImage(string offset)
+        {
+            if (MainScrollEvent.IsLoading)
+                return;
+
+            if (Methods.CheckConnectivity())
+            {
+                MainScrollEvent.IsLoading = true;
+                int countList = MAdapter.MyPhotosList.Count;
+                (int apiStatus, var respond) = await RequestsAsync.Album.GetPostByType(UserDetails.UserId, "photos", "10", offset);
+                if (apiStatus != 200 || !(respond is GetImagesObject result) || result.Data == null)
+                {
+                    MainScrollEvent.IsLoading = false;
+                    Methods.DisplayReportResult(this, respond);
+                }
+                else
+                {
+                    var respondList = result.Data?.Count;
+                    if (respondList > 0)
+                    {
+                        result.Data.RemoveAll(w => string.IsNullOrEmpty(w.PostFileFull));
+
+                        if (countList > 0)
+                        {
+                            foreach (var item in from item in result.Data let check = MAdapter.MyPhotosList.FirstOrDefault(a => a.Id == item.Id) where check == null select item)
+                            {
+                                MAdapter.MyPhotosList.Add(item);
+                            }
+
+                            RunOnUiThread(() => { MAdapter.NotifyItemRangeInserted(countList, MAdapter.MyPhotosList.Count - countList); });
+                        }
+                        else
+                        {
+                            MAdapter.MyPhotosList = new ObservableCollection<PostDataObject>(result.Data);
+                            RunOnUiThread(() => { MAdapter.NotifyDataSetChanged(); });
+                        }
+                    }
+                    else
+                    {
+                        if (MAdapter.MyPhotosList.Count > 10 && !MRecycler.CanScrollVertically(1))
+                            Toast.MakeText(this, GetText(Resource.String.Lbl_NoMorePhoto), ToastLength.Short).Show();
+                    }
+                }
+                 
+                RunOnUiThread(ShowEmptyPage);
+            }
+            else
+            {
+                Inflated = EmptyStateLayout.Inflate();
+                EmptyStateInflater x = new EmptyStateInflater();
+                x.InflateLayout(Inflated, EmptyStateInflater.Type.NoConnection);
+                if (!x.EmptyStateButton.HasOnClickListeners)
+                {
+                    x.EmptyStateButton.Click += null;
+                    x.EmptyStateButton.Click += EmptyStateButtonOnClick;
+                }
+
+                Toast.MakeText(this, GetString(Resource.String.Lbl_CheckYourInternetConnection), ToastLength.Short).Show();
+                MainScrollEvent.IsLoading = false;
+            }
+        }
+
+        private void ShowEmptyPage()
+        {
+            try
+            {
+                MainScrollEvent.IsLoading = false; 
+                SwipeRefreshLayout.Refreshing = false;
+
+                if (MAdapter.MyPhotosList.Count > 0)
+                {
+                    MRecycler.Visibility = ViewStates.Visible;
+                    EmptyStateLayout.Visibility = ViewStates.Gone;
+                }
+                else
+                {
+                    MRecycler.Visibility = ViewStates.Gone;
+
+                    Inflated ??= EmptyStateLayout.Inflate();
+
+                    EmptyStateInflater x = new EmptyStateInflater();
+                    x.InflateLayout(Inflated, EmptyStateInflater.Type.NoPhoto);
+                    if (!x.EmptyStateButton.HasOnClickListeners)
+                    {
+                        x.EmptyStateButton.Click += null;
+                    }
+                    EmptyStateLayout.Visibility = ViewStates.Visible;
+                }
+            }
+            catch (Exception e)
+            {
+                MainScrollEvent.IsLoading = false;
+                SwipeRefreshLayout.Refreshing = false;
+                Console.WriteLine(e);
+            }
+        }
+
+        //No Internet Connection 
+        private void EmptyStateButtonOnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                StartApiService();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+    }
+} 
