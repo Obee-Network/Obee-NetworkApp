1cea2a3622e1c2b71b51818d9c3cf2a01b7f1043
W-Tenghua <939731967@qq.com>
2020-07-19 10:14:26 +0800

    [add CommonThingsActivity]

---
+++ trueapp/Activities/CommonThings/CommonThingsActivity.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Ads;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Bumptech.Glide.Integration.RecyclerView;
+using Bumptech.Glide.Util;
+using ObeeNetwork.Activities.CommonThings.Adapters;
+using ObeeNetwork.Helpers.Ads;
+using ObeeNetwork.Helpers.Controller;
+using ObeeNetwork.Helpers.Utils;
+using ObeeNetworkClient.Classes.Global;
+using ObeeNetworkClient.Requests;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace ObeeNetwork.Activities.CommonThings
+{
+    [Activity(Icon = "@mipmap/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.Locale | ConfigChanges.UiMode | ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class CommonThingsActivity : AppCompatActivity
+    {
+        #region Variables Basic
+
+        private CommonThingsAdapter MAdapter;
+        private SwipeRefreshLayout SwipeRefreshLayout;
+        private RecyclerView MRecycler;
+        private GridLayoutManager LayoutManager;
+        private ViewStub EmptyStateLayout;
+        private View Inflated;
+        private RecyclerViewOnScrollListener MainScrollEvent;
+        private AdView MAdView;
+
+        #endregion
+
+        #region General
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                SetTheme(AppSettings.SetTabDarkTheme ? Resource.Style.MyTheme_Dark_Base : Resource.Style.MyTheme_Base);
+
+                Methods.App.FullScreenApp(this);
+
+                // Create your application here
+                SetContentView(Resource.Layout.RecyclerDefaultLayout);
+
+                //Get Value And Set Toolbar
+                InitComponent();
+                InitToolbar();
+                SetRecyclerViewAdapters();
+
+                StartApiService();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnResume()
+        {
+            try
+            {
+                base.OnResume();
+                AddOrRemoveEvent(true);
+                MAdView?.Resume();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnPause()
+        {
+            try
+            {
+                base.OnPause();
+                AddOrRemoveEvent(false);
+                MAdView?.Pause();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public override void OnTrimMemory(TrimMemory level)
+        {
+            try
+            {
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+                base.OnTrimMemory(level);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public override void OnLowMemory()
+        {
+            try
+            {
+                GC.Collect(GC.MaxGeneration);
+                base.OnLowMemory();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            try
+            {
+                DestroyBasic();
+
+                base.OnDestroy();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+        #region Menu 
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    Finish();
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        } 
+        #endregion
+
+        #region Functions
+
+        private void InitComponent()
+        {
+            try
+            {
+                MRecycler = (RecyclerView)FindViewById(Resource.Id.recyler);
+                EmptyStateLayout = FindViewById<ViewStub>(Resource.Id.viewStub);
+
+                SwipeRefreshLayout = (SwipeRefreshLayout)FindViewById(Resource.Id.swipeRefreshLayout);
+                SwipeRefreshLayout.SetColorSchemeResources(Android.Resource.Color.HoloBlueLight, Android.Resource.Color.HoloGreenLight, Android.Resource.Color.HoloOrangeLight, Android.Resource.Color.HoloRedLight);
+                SwipeRefreshLayout.Refreshing = true;
+                SwipeRefreshLayout.Enabled = true;
+                SwipeRefreshLayout.SetProgressBackgroundColorSchemeColor(AppSettings.SetTabDarkTheme ? Color.ParseColor("#424242") : Color.ParseColor("#f7f7f7"));
+ 
+
+                MAdView = FindViewById<AdView>(Resource.Id.adView);
+                AdsGoogle.InitAdView(MAdView, MRecycler);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void InitToolbar()
+        {
+            try
+            {
+                var toolBar = FindViewById<Toolbar>(Resource.Id.toolbar);
+                if (toolBar != null)
+                {
+                    toolBar.Title = GetText(Resource.String.Lbl_CommonThings);
+
+                    toolBar.SetTitleTextColor(Color.White);
+                    SetSupportActionBar(toolBar);
+                    SupportActionBar.SetDisplayShowCustomEnabled(true);
+                    SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+                    SupportActionBar.SetHomeButtonEnabled(true);
+                    SupportActionBar.SetDisplayShowHomeEnabled(true);
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void SetRecyclerViewAdapters()
+        {
+            try
+            {
+                MAdapter = new CommonThingsAdapter(this)
+                {
+                    CommonThingsList = new ObservableCollection<CommonThingsObject>()
+                };
+                LayoutManager = new GridLayoutManager(this, 2);
+                //LayoutManager.SetSpanSizeLookup(new MySpanSizeLookup(4, 1, 1)); //5, 1, 2 
+                MRecycler.SetLayoutManager(LayoutManager);
+                MRecycler.HasFixedSize = true;
+                MRecycler.SetItemViewCacheSize(10);
+                MRecycler.GetLayoutManager().ItemPrefetchEnabled = true;
+                var sizeProvider = new FixedPreloadSizeProvider(10, 10);
+                var preLoader = new RecyclerViewPreloader<CommonThingsObject>(this, MAdapter, sizeProvider, 10);
+                MRecycler.AddOnScrollListener(preLoader);
+                MRecycler.SetAdapter(MAdapter);
+
+                RecyclerViewOnScrollListener xamarinRecyclerViewOnScrollListener = new RecyclerViewOnScrollListener(LayoutManager);
+                MainScrollEvent = xamarinRecyclerViewOnScrollListener;
+                MainScrollEvent.LoadMoreEvent += MainScrollEventOnLoadMoreEvent;
+                MRecycler.AddOnScrollListener(xamarinRecyclerViewOnScrollListener);
+                MainScrollEvent.IsLoading = false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void AddOrRemoveEvent(bool addEvent)
+        {
+            try
+            {
+                // true +=  // false -=
+                if (addEvent)
+                {
+                    MAdapter.ItemClick += MAdapterOnItemClick;
+                    SwipeRefreshLayout.Refresh += SwipeRefreshLayoutOnRefresh; 
+                }
+                else
+                {
+                    MAdapter.ItemClick -= MAdapterOnItemClick;
+                    SwipeRefreshLayout.Refresh -= SwipeRefreshLayoutOnRefresh; 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+         
+        private void DestroyBasic()
+        {
+            try
+            {
+                MAdView?.Destroy();
+
+                MAdapter = null;
+                SwipeRefreshLayout = null;
+                MRecycler = null;
+                EmptyStateLayout = null;
+                Inflated = null;
+                MainScrollEvent = null;
+                MAdView = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        //Refresh
+        private void SwipeRefreshLayoutOnRefresh(object sender, EventArgs e)
+        {
+            try
+            {
+                MAdapter.CommonThingsList.Clear();
+                MAdapter.NotifyDataSetChanged();
+
+                MainScrollEvent.IsLoading = false;
+
+                StartApiService();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        //Scroll
+        private void MainScrollEventOnLoadMoreEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                //Code get last id where LoadMore >>
+                var item = MAdapter.CommonThingsList.LastOrDefault();
+                if (item != null && !string.IsNullOrEmpty(item.UserId) && !MainScrollEvent.IsLoading)
+                    StartApiService(item.UserId);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private void MAdapterOnItemClick(object sender, CommonThingsAdapterClickEventArgs e)
+        {
+            try
+            {
+                var item = MAdapter.GetItem(e.Position);
+                if (item != null)
+                {
+                    ObeeNetworkTools.OpenProfile(this, item.UserData.UserId, item.UserData);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+        #region Load CommonThings 
+
+        private void StartApiService(string offset = "")
+        {
+            if (!Methods.CheckConnectivity())
+                Toast.MakeText(this, GetString(Resource.String.Lbl_CheckYourInternetConnection), ToastLength.Short).Show();
+            else
+                PollyController.RunRetryPolicyFunction(new List<Func<Task>> { () => LoadCommonThingsAsync(offset) });
+        }
+
+        private async Task LoadCommonThingsAsync(string offset)
+        {
+            if (MainScrollEvent.IsLoading)
+                return;
+
+            if (Methods.CheckConnectivity())
+            {
+                MainScrollEvent.IsLoading = true; 
+                int countList = MAdapter.CommonThingsList.Count;
+                (int apiStatus, var respond) = await RequestsAsync.Global.FetchCommonThings("10", offset);
+                if (apiStatus != 200 || !(respond is FetchCommonThingsObject result) || result.Data == null)
+                {
+                    MainScrollEvent.IsLoading = false;
+                    Methods.DisplayReportResult(this, respond);
+                }
+                else
+                {
+                    var respondList = result.Data.Count;
+                    if (respondList > 0)
+                    {
+                        if (countList > 0)
+                        {
+                            foreach (var item in from item in result.Data let check = MAdapter.CommonThingsList.FirstOrDefault(a => a.UserId == item.UserId) where check == null select item)
+                            {
+                                MAdapter.CommonThingsList.Add(item);
+                            }
+
+                            RunOnUiThread(() => { MAdapter.NotifyItemRangeInserted(countList, MAdapter.CommonThingsList.Count - countList); });
+                        }
+                        else
+                        {
+                            MAdapter.CommonThingsList = new ObservableCollection<CommonThingsObject>(result.Data);
+                            RunOnUiThread(() => { MAdapter.NotifyDataSetChanged(); });
+                        } 
+                    }
+                    else
+                    {
+                        if (MAdapter.CommonThingsList.Count > 10 && !MRecycler.CanScrollVertically(1))
+                            Toast.MakeText(this, GetText(Resource.String.Lbl_No_more_users), ToastLength.Short).Show();
+                    }
+                }
+
+                RunOnUiThread(ShowEmptyPage);
+            }
+            else
+            {
+                Inflated = EmptyStateLayout.Inflate();
+                EmptyStateInflater x = new EmptyStateInflater();
+                x.InflateLayout(Inflated, EmptyStateInflater.Type.NoConnection);
+                if (!x.EmptyStateButton.HasOnClickListeners)
+                {
+                    x.EmptyStateButton.Click += null;
+                    x.EmptyStateButton.Click += EmptyStateButtonOnClick;
+                }
+
+                Toast.MakeText(this, GetString(Resource.String.Lbl_CheckYourInternetConnection), ToastLength.Short).Show();
+                MainScrollEvent.IsLoading = false;
+            }
+        }
+
+        private void ShowEmptyPage()
+        {
+            try
+            {
+                MainScrollEvent.IsLoading = false; 
+                SwipeRefreshLayout.Refreshing = false;
+
+                if (MAdapter.CommonThingsList.Count > 0)
+                {
+                    MRecycler.Visibility = ViewStates.Visible;
+                    EmptyStateLayout.Visibility = ViewStates.Gone;
+                }
+                else
+                {
+                    MRecycler.Visibility = ViewStates.Gone;
+
+                    Inflated ??= EmptyStateLayout.Inflate();
+
+                    EmptyStateInflater x = new EmptyStateInflater();
+                    x.InflateLayout(Inflated, EmptyStateInflater.Type.NoCommonThings);
+                    if (!x.EmptyStateButton.HasOnClickListeners)
+                    {
+                        x.EmptyStateButton.Click += null;
+                    }
+                    EmptyStateLayout.Visibility = ViewStates.Visible;
+                }
+            }
+            catch (Exception e)
+            {
+                MainScrollEvent.IsLoading = false;
+                SwipeRefreshLayout.Refreshing = false;
+                Console.WriteLine(e);
+            }
+        }
+
+        //No Internet Connection 
+        private void EmptyStateButtonOnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                StartApiService();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        #endregion
+
+    }
+}
